7ea8656300feafe94496dc37d80b98c6d4fd1d64
rladygks329 <rladygks329@gmail.com>
2023-02-03 18:27:46 +0900

    Add: React-Router
    사용된 명령어: npm i react-router-dom
    라우팅 페이지 추가: signup, signin, main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./pages/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
