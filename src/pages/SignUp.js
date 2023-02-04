51d24ac4709f9f03d4e2e276283438ffcc588cda
rladygks329 <rladygks329@gmail.com>
2023-02-04 14:31:47 +0900

    Feat: Assignment 1

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,48 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useState } from "react";
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [signUpData, setSignUpData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form>
+      <input
+        data-testid="email-input"
+        type="text"
+        placeholder="이메일을 입력하세요"
+        name="email"
+        value={signUpData.email}
+        onChange={handleChange}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="비밀번호를 입력하세요"
+        name="password"
+        value={signUpData.password}
+        onChange={handleChange}
+      />
+      <button
+        data-testid="signup-button"
+        disabled={
+          !(signUpData.email.includes("@") && signUpData.password.length > 7)
+        }
+        onSubmit={handleSubmit}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default SignUp;
