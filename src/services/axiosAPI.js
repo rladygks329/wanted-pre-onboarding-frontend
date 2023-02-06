e1262951e51a7c647f8bba61960c421edac9d31c
rladygks329 <rladygks329@gmail.com>
2023-02-06 23:16:26 +0900

    Fix: header
    Authorization 추가, 토큰이 없을 때 실행안되는 버그 수정

--- src/services/axiosAPI.js
+++ src/services/axiosAPI.js
@@ -2,6 +2,16 @@ import axios from "axios";
 
 const axiosAPI = axios.create({
   baseURL: "https://pre-onboarding-selection-task.shop/",
+  headers: {
+    Authorization: "Bearer ",
+    "Content-Type": "application/json",
+  },
+});
+
+axiosAPI.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token") ?? "";
+  config.headers.Authorization = "Bearer ".concat(token);
+  return config;
 });
 
 export default axiosAPI;
