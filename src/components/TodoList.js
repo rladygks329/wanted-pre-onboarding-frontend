9705324723d860307b77a9cf178811e980592fa0
rladygks329 <rladygks329@gmail.com>
2023-02-07 23:36:14 +0900

    Feat: Assignment 8
    수정 삭제 버튼 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,6 +15,8 @@ const TodoList = React.memo((props) => {
         <input type="checkbox" checked={completed} onChange={handleChange} />
         <span>{content}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 });
