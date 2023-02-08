cdb21a050834ee3e8179507b18c524828b09c3b8
rladygks329 <rladygks329@gmail.com>
2023-02-08 22:34:31 +0900

    delete: main
    로그인 페이지가 default로 되도록 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import Main from "./pages/Main";
 import ToDo from "./pages/ToDo";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
         <Route path="todo" element={<ToDo />} />
