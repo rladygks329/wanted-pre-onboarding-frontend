f448165c15beb0c3932773fedef9c78a91ba8938
rladygks329 <rladygks329@gmail.com>
2023-02-08 22:32:58 +0900

    Assignment 10
    수정 기능 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,28 +1,82 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 const TodoList = React.memo((props) => {
   const { id, todo, isCompleted } = props.data;
   const { update, remove } = props;
-  const [content, setContent] = useState(todo);
-  const [completed, setCompleted] = useState(isCompleted);
+  const [todoItem, setTodoItem] = useState({
+    content: todo,
+    user_input: todo,
+    isEditing: false,
+    isCompleted: isCompleted,
+  });
+  const toggleComplete = (e) => {
+    update(id, todoItem.content, !todoItem.isCompleted);
+    setTodoItem({
+      ...todoItem,
+      isCompleted: !todoItem.isCompleted,
+    });
+  };
+  const toggleEditing = (e) => {
+    setTodoItem({
+      ...todoItem,
+      isEditing: !todoItem.isEditing,
+    });
+  };
   const handleChange = (e) => {
-    update(id, todo, !isCompleted).then(setCompleted(!completed));
+    setTodoItem({ ...todoItem, user_input: e.target.value });
+  };
+  const handleCancel = (e) => {
+    setTodoItem({
+      ...todoItem,
+      user_input: todoItem.content,
+      isEditing: false,
+    });
+  };
+  const handleEdit = (e) => {
+    update(id, todoItem.user_input, todoItem.isCompleted);
+    setTodoItem({
+      ...todoItem,
+      content: todoItem.user_input,
+      isEditing: false,
+    });
+  };
+  const handleRemove = (e) => {
+    remove(id);
   };
   return (
     <li>
       <label>
-        <input type="checkbox" checked={completed} onChange={handleChange} />
-        <span>{content}</span>
+        <input
+          type="checkbox"
+          checked={todoItem.isCompleted}
+          onChange={toggleComplete}
+        />
       </label>
-      <button data-testid="modify-button">수정</button>
-      <button
-        data-testid="delete-button"
-        onClick={() => {
-          remove(id);
-        }}
-      >
-        삭제
-      </button>
+      {todoItem.isEditing ? (
+        <Fragment>
+          <input
+            data-testid="modify-input"
+            value={todoItem.user_input}
+            onChange={handleChange}
+          />
+          <button data-testid="submit-button" onClick={handleEdit}>
+            제출
+          </button>
+          <button data-testid="cancel-button" onClick={handleCancel}>
+            취소
+          </button>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <span>{todoItem.content}</span>
+          <button data-testid="modify-button" onClick={toggleEditing}>
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={handleRemove}>
+            삭제
+          </button>
+        </Fragment>
+      )}
     </li>
   );
 });
