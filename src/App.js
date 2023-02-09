97e51795c4879e4d39ac4ca4e5bdea67f018c38c
rladygks329 <rladygks329@gmail.com>
2023-02-10 00:08:17 +0900

    Chore: 깃허브 페이지 배포

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import ToDo from "./pages/ToDo";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="todo" element={<ToDo />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/todo" element={<ToDo />} />
       </Routes>
     </BrowserRouter>
   );
