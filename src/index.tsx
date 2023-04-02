a7e0410ee8611dc5f90bc0b3e77b5385e92dbbf4
Kim Yo Han <rladygks329@gmail.com>
2023-04-03 00:24:22 +0900

    feat: reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+`;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </React.StrictMode>
 );
