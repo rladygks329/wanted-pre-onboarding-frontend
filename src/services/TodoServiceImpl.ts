c89c34b83dcedc662c5fd40c9e528aad94842335
Kim Yo Han <rladygks329@gmail.com>
2023-04-11 22:48:26 +0900

    feat: TodoService

---
+++ src/services/TodoServiceImpl.ts
@@ -0,0 +1,50 @@
+import { Todo } from '../types/Todo';
+import HttpClient from './interfaces/HttpClient';
+import TodoService from './interfaces/TodoService';
+
+class TodoServiceImpl implements TodoService {
+  httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    return this.httpClient
+      .fetch('/todos', {
+        method: 'GET',
+      })
+      .then((todos: Todo[]) => {
+        return todos;
+      });
+  }
+
+  async createTodo(todo: string): Promise<Todo> {
+    return this.httpClient
+      .fetch('/todos', {
+        method: 'POST',
+        body: JSON.stringify({ todo }),
+      })
+      .then((todo: Todo) => {
+        return todo;
+      });
+  }
+
+  async deleteTodo(id: number): Promise<void> {
+    return this.httpClient.fetch(`/todos${id}`, { method: 'DELETE' });
+  }
+
+  async updateTodo(
+    id: number,
+    todo: string,
+    isCompleted: boolean
+  ): Promise<Todo> {
+    return this.httpClient
+      .fetch(`/todos${id}`, { todo, isCompleted })
+      .then((Todo: Todo) => {
+        return Todo;
+      });
+  }
+}
+
+export { TodoServiceImpl };
---
+++ src/services/interfaces/TodoService.ts
@@ -0,0 +1,13 @@
+import { Todo } from '../../types/Todo';
+import HttpClient from './HttpClient';
+
+interface TodoService {
+  httpClient: HttpClient;
+
+  createTodo(todo: string): Promise<Todo>;
+  getTodos(): Promise<Todo[]>;
+  updateTodo(id: number, todo: String, isCompleted: boolean): Promise<Todo>;
+  deleteTodo(id: number): Promise<void>;
+}
+
+export default TodoService;
