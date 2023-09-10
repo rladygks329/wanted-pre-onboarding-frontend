e8e42433341f48b5951d1b5ce1d386536533d497
Kim Yo Han <rladygks329@gmail.com>
2023-09-10 22:34:28 +0900

    feat: Navbar
    로그인 유무에 따라 Navbar에 로그인 / 로그아웃이 나타나도록 변경

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import AuthService from '../types/AuthService';
 
 const AuthContext = createContext({
@@ -9,7 +9,13 @@ const AuthContext = createContext({
   signUp: async (email: string, password: string) => {
     return;
   },
+
+  signOff: async () => {
+    return;
+  },
+  isLoggedIn: false,
 });
+
 export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({
@@ -19,11 +25,22 @@ export function AuthProvider({
   children: React.ReactNode;
   authService: AuthService;
 }) {
-  const signIn = authService.signIn.bind(authService);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const signIn = async (email: string, password: string) => {
+    await authService.signIn(email, password);
+    setIsLoggedIn(authService.tokenRepo.get() !== '');
+    return;
+  };
   const signUp = authService.signUp.bind(authService);
 
+  const signOff = async () => {
+    await authService.signOff();
+    setIsLoggedIn(authService.tokenRepo.get() !== '');
+  };
+
   return (
-    <AuthContext.Provider value={{ signIn, signUp }}>
+    <AuthContext.Provider value={{ signIn, signUp, signOff, isLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
