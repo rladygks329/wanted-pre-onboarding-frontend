01b6d6c97f06b8d1160c2898bd300907ae24a426
Kim Yo Han <rladygks329@gmail.com>
2023-11-11 14:57:43 +0900

    fix: domain address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const ACCESS_TOKEN_KEY = 'access_token';
-export const BASE_URL = 'https://www.pre-onboarding-selection-task.shop';
+export const BASE_URL = 'https://www.pre-onboarding-selection-task.store';
